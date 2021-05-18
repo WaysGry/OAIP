223aa662780d73390b06c06d74052db5f30fedc9
WaysGry <84223582+WaysGry@users.noreply.github.com>
2021-05-18 10:30:32 +0500

    Add files via upload

---
+++ Задача на 3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+       {
+                Console.Write("Введите Начало промежутка: ");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("Введите конец промежутка: ");
+                int b = int.Parse(Console.ReadLine());
+                Console.Write("Введите кратное: ");
+                int c = int.Parse(Console.ReadLine());
+                if (a < b)
+                {
+                    int kol = 0, proiz = 1;
+                    for (int i = a; i <= b; i++)
+                    {
+                        if (i % c == 0 )
+                        {
+                            kol++;
+                            proiz *= i;
+                        }
+                    }
+                    Console.WriteLine("В промежутке чисел кратных c = " + kol);
+                    Console.WriteLine("Их произведение =" +proiz);
+                }
+            
+        }
+           
+    }
+}
+
